9c84aa9eba4f0979ecf2e9a0711c17ab31279961
Nelson <nelson@hotmil.com>
2020-04-20 00:55:33 -0400

    commit momentaneo, queda pendiente terminar el banner

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './style/style.scss'
+import Header from './Header'
+import Banner from './Banner'
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Banner />
+    </>
+  )
+}
+
+export default App;
---
+++ src/Banner.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+// import fondo from 'https://cdn.pixabay.com/index/2020/04/14/13-18-37-791_1440x480.jpg'
+
+const Banner = () => {
+  return (
+    <div className="main-banner">
+      <div className="main-banner--content">
+        <h3>Increíbles imágenes gratis para descargar</h3>
+        <p>Nuestro banco de imágenes tiene más de 1.7 millones de imágenes y videos compartidos por nuestra talentosa comunidad.</p>
+      </div>
+    </div>
+  )
+}
+
+export default Banner
